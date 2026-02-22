c80fcc1c13dbad27eb3ec64a0b4990453dbaa5b7
Andy Jackson <snadrus@gmail.com>
2026-02-22 17:51:49 -0600

    cleanups

--- internal/ffmpeglib/proc_linux.go
+++ internal/ffmpeglib/proc_linux.go
@@ -2,13 +2,9 @@ package ffmpeglib
 
 import (
 	"os/exec"
-	"syscall"
 )
 
 func configureCmd(cmd *exec.Cmd, bin string, args []string) {
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Pdeathsig: syscall.SIGTERM,
-	}
 	if nicePath, err := exec.LookPath("nice"); err == nil {
 		cmd.Path = nicePath
 		cmd.Args = append([]string{"nice", "-n", "19", "ionice", "-c", "2", "-n", "7", bin}, args...)
